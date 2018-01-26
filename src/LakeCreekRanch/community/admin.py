82ce252e9f89313b2a9281e9fcf4205a5debd2fc
Derek Howard <howardderekl@gmail.com>
2018-01-26 10:07:10 -0800

    Fixing the data types to better fields Adjusting the admin tool to show what fields to display

--- src/LakeCreekRanch/community/admin.py
+++ src/LakeCreekRanch/community/admin.py
@@ -2,6 +2,35 @@ from django.contrib import admin
 
 from .models import DevelopmentPhase, Lot, HomeForSale
 
-admin.site.register(DevelopmentPhase)
-admin.site.register(Lot)
-admin.site.register(HomeForSale)
+@admin.register(DevelopmentPhase)
+class DevelopmentPhaseAdmin(admin.ModelAdmin):
+    """Declaration of what to display for the DevelopmentPhases in the Admin Tool"""
+    list_display = ('name',
+                    'start_date',
+                    'complete_date',
+                    'hide_from_website')
+    list_editable = ('hide_from_website',)
+
+@admin.register(Lot)
+class LotAdmin(admin.ModelAdmin):
+    """Declaration of what to display for the Lots in the Admin Tool"""
+    list_display = ('name',
+                    'description',
+                    'total_acres',
+                    'sale_status',
+                    'development_phase')
+    list_editable = ('sale_status',)
+
+@admin.register(HomeForSale)
+class HomeForSaleAdmin(admin.ModelAdmin):
+    """Declaration of what to display for the HomesForSale in the Admin Tool"""
+    list_display = ('id',
+                    'address',
+                    'total_sqr_feet',
+                    'bedroom_count',
+                    'bathroom_count',
+                    'hide_from_website',
+                    'completion_date',
+                    'sale_status',
+                    'lot')
+    list_editable = ('hide_from_website', 'sale_status')
--- src/LakeCreekRanch/community/models.py
+++ src/LakeCreekRanch/community/models.py
@@ -8,12 +8,12 @@ FOR_SALE_CHOICES = (
 
 class DevelopmentPhase(models.Model):
     name = models.CharField(max_length=250)
-    start_date = models.DateTimeField()
-    complete_date = models.DateTimeField()
+    start_date = models.DateField()
+    complete_date = models.DateField()
     hide_from_website = models.BooleanField()
 
     def __str__(self):
-        return "%s the development phase" % self.name
+        return "%s" % self.name
 
 
 class Lot(models.Model):
@@ -25,20 +25,20 @@ class Lot(models.Model):
     development_phase = models.ForeignKey(DevelopmentPhase, on_delete=models.CASCADE)
 
     def __str__(self):
-        return "%s the lot" % self.name
+        return "%s" % self.name
 
 
 class HomeForSale(models.Model):
-    description = models.CharField(max_length=2000)
+    description = models.TextField()
     address = models.CharField(max_length=100)
-    mls_url = models.CharField(max_length=250)
+    mls_url = models.URLField()
     total_sqr_feet = models.IntegerField()
     bedroom_count = models.IntegerField()
     bathroom_count = models.DecimalField(max_digits=3, decimal_places=2)
     hide_from_website = models.BooleanField(default=False)
-    completion_date = models.DateTimeField()
+    completion_date = models.DateField()
     sale_status = models.CharField(max_length=1, default='F', choices=FOR_SALE_CHOICES)
     lot = models.ForeignKey(Lot, on_delete=models.CASCADE)
 
     def __str__(self):
-        return "%s the home for sale" % self.address
+        return "HomeId: {0} @ {1}".format(self.id, self.address)
